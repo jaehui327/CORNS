3be46d8130eedc38cb80c5a2ebe8b1f6129a7248
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-01-17 17:18:44 +0900

    🎉 [DEVEOLOP BE] begin a project

---
+++ backend/src/test/java/com/w6w/corns/CornsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w6w.corns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
