96ab6d3d9f0664fbe2de79fb6c8a6b4848751348
isks2 <isks2@naver.com>
2023-01-17 16:54:37 +0900

    :tada: [DEVELOP FE] begin a project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>CORNS의 첫페이지 입니다!!</p>
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
