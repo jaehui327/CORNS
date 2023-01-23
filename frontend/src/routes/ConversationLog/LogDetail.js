74a8f7e77c59a6cef38f2460b0eea89a4aad146c
haun <ryuhaun0321@gmail.com>
2023-01-23 16:40:15 +0900

    [DEVELOP FE] ✅ add log_detail component route

--- frontend/src/routes/ConversationLog/LogDetail.js
+++ frontend/src/routes/ConversationLog/LogDetail.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function LogDetail() {
-    return <h5>LogDetail</h5>
+function LogDetail({match}) {
+    const {room_no} = match.params
+
+    // fetch 방 상세 정보 불러오기
+    
+    return (
+        <>
+        <h5>LogDetail</h5>
+        {room_no}
+        </>
+    )
 }
 
 export default LogDetail;
