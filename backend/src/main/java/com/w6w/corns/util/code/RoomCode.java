ee0bcf23c1457ce14d982b90b91a599840ea28c5
DESKTOP-DOGVPUB\SSAFY <bhi991228@naver.com>
2023-01-25 17:25:56 +0900

    ✅ add enum files and BaseTime

---
+++ backend/src/main/java/com/w6w/corns/util/code/RoomCode.java
@@ -0,0 +1,16 @@
+package com.w6w.corns.util.code;
+
+public enum RoomCode {
+
+    ROOM_WAITING(100), ROOM_START(101), ROOM_END(102);
+
+    private final int code;
+
+    RoomCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
