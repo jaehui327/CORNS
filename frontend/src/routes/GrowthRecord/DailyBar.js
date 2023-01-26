e5f3401cae76baf8fc04c34077b1266e88f5548b
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-01-26 13:07:15 +0900

    ⚡error fix

--- frontend/src/routes/GrowthRecord/DailyBar.js
+++ frontend/src/routes/GrowthRecord/DailyBar.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function DailyBar() {
-  return (
-    <div>DailyBar</div>
-  )
+  return <div>DailyBar</div>;
 }
 
 export default DailyBar;
--- frontend/src/routes/GrowthRecord/WeeklyLiner.js
+++ frontend/src/routes/GrowthRecord/WeeklyLiner.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function WeeklyLiner() {
-  return (
-    <div>WeeklyLiner</div>
-  )
+  return <div>WeeklyLiner</div>;
 }
 
 export default WeeklyLiner;
