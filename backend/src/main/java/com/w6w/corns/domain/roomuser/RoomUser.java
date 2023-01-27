b12dd7c9874c9f77a30b6cc3f11795d917562b94
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-01-27 11:17:11 +0900

    ✅ [BE] entity, repository 생성 - Room, RoomBookmark, RoomUser, Subject, Word

---
+++ backend/src/main/java/com/w6w/corns/domain/roomuser/RoomUser.java
@@ -0,0 +1,36 @@
+package com.w6w.corns.domain.roomuser;
+
+import com.w6w.corns.util.BaseTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@DynamicInsert
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Entity
+public class RoomUser extends BaseTime {
+
+    @Id()
+    private int roomUserNo;
+
+    private int roomNo;
+
+    private int userId;
+
+    private int roomUserCd;
+
+    private String connectionId;
+
+    private String recordId;
+
+    private String token;
+
+    private String scriptUrl;
+
+}
