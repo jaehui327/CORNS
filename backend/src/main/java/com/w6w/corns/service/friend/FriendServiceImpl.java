ee2100a9b4d3bd5d9e7b6b19beb3a4c748d86403
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-01-27 12:21:52 +0900

    ✅ [BE] service 생성 - conversation log, evaluation, friend, growth, rank, room, word

---
+++ backend/src/main/java/com/w6w/corns/service/friend/FriendServiceImpl.java
@@ -0,0 +1,4 @@
+package com.w6w.corns.service.friend;
+
+public class FriendServiceImpl implements FriendService {
+}
---
+++ backend/src/main/java/com/w6w/corns/service/rank/RankService.java
@@ -0,0 +1,4 @@
+package com.w6w.corns.service.rank;
+
+public interface RankService {
+}
