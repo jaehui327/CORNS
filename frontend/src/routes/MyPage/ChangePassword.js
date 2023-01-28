10e851cc96437e4846636ce8ffc85dff7e218d5c
haun <ryuhaun0321@gmail.com>
2023-01-28 17:18:59 +0900

    🎨 mypage publishing

--- frontend/src/routes/MyPage/ChangePassword.js
+++ frontend/src/routes/MyPage/ChangePassword.js
@@ -1,9 +1,38 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
 function ChangePassword() {
   return (
     <>
       <h2>비밀번호 수정</h2>
+
+      <Box sx={{ fontSize: "20px" }}>
+        <Box>
+          <h5>현재 비밀번호</h5>
+          <input type="text" />
+        </Box>
+        
+        <Box>
+          <h5>비밀번호 재확인</h5>
+          <input type="text" />
+        </Box>
+
+        <Box>
+          <h5>새 비밀번호</h5>
+          <input type="text" />
+          <Button
+              sx={{
+                border: "3px solid #111",
+                color: "#111111",
+                backgroundColor: "#FFC804",
+              }}
+            >
+              수정
+            </Button>
+        </Box>
+      </Box>
     </>
   );
 }
--- frontend/src/routes/MyPage/ChangeProfile.js
+++ frontend/src/routes/MyPage/ChangeProfile.js
@@ -1,9 +1,64 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button'
+
 function ChangeProfile() {
   return (
     <>
       <h2>회원 수정</h2>
+
+      <Box sx={{ fontSize: "20px" }}>
+        <Box>
+          <h5>아이디</h5>
+          <input type="text" />
+        </Box>
+
+        <Box>
+          <h5>닉네임</h5>
+          <input type="text" />
+          <Button
+              sx={{
+                border: "3px solid #111",
+                color: "#111111",
+                backgroundColor: "#FFC804",
+              }}
+            >
+              수정
+            </Button>
+        </Box>
+
+        <Box>
+          <h5>이메일</h5>
+          <input type="text" />
+        </Box>
+
+        <Box>
+          <h5>프로필 이미지</h5>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "3px solid #111",
+              width: "50%",
+              height: "240px",
+            }}
+          >
+            <p>이미지를 끌어서 업로드하세요.</p>
+            <Button
+              sx={{
+                border: "3px solid #111",
+                color: "#111111",
+                backgroundColor: "#FFC804",
+              }}
+            >
+              파일 찾아서 올리기
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 }
