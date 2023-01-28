9447b9c853328d9621c6788e679207c841356e7e
haun <ryuhaun0321@gmail.com>
2023-01-28 14:51:12 +0900

    🎨 FriendRequest component publishing

--- frontend/src/components/Community/FriendRequest.js
+++ frontend/src/components/Community/FriendRequest.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import RequestList from './RequestList';
 
 function FriendRequest() {
+  const users = [
+    {
+      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      nickname: "isk2",
+      user_id: 1000,
+    },
+    {
+      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      nickname: "isk2",
+      user_id: 1000,
+    },
+    {
+      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      nickname: "isk2",
+      user_id: 1000,
+    },
+    {
+      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      nickname: "isk2",
+      user_id: 1000,
+    },
+  ]
+  
+  
   return (
     <>
       <h3>친구신청</h3>
+      <RequestList users={users}/>
     </>
   )
 }
