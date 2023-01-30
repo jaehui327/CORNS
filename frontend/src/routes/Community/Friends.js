dfb3416992ada9bfcd61c6788ce0a5bcd62dc12e
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-01-30 17:57:52 +0900

    🎨 searchuser % friends publishing

--- frontend/src/routes/Community/Friends.js
+++ frontend/src/routes/Community/Friends.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import FriendList from '../../components/Community/FriendList';
 import FriendRequest from '../../components/Community/FriendRequest';
+import Box from "@mui/material/Box";
 
 function Friends() {
   return (
     <>
       <h2>친구</h2>
-      <FriendRequest />
-      <FriendList />
+      <Box>
+        <FriendRequest />
+      </Box>
+      <Box>
+        <FriendList />
+      </Box>
+      
     </>
   );
 
