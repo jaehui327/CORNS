2d79a5896598e1460580fad1cd2e30817c5fa240
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-01-30 12:44:18 +0900

    ⚡️modify sidebar gap

--- frontend/src/components/GlobalComponents/Sidebar.js
+++ frontend/src/components/GlobalComponents/Sidebar.js
@@ -7,15 +7,23 @@ const Sidebar = ({ Items, ...rest }) => {
   return (
     <div
       css={css`
+        display: flex;
+        flex-direction: column;
         box-sizing: border-box;
-        background: #FFC804;
+        background: #ffc804;
         border: 3px solid #111111;
         padding: 32px 0 100px 32px;
+        gap: 40px;
       `}
     >
       {Items.map((item, index) => {
         return (
-          <NavLink to={item.route} key={index}  style={{ textDecoration: "none", color: "black" }} activeStyle={{fontWeight: 'bold'}}>
+          <NavLink
+            to={item.route}
+            key={index}
+            style={{ textDecoration: "none", color: "black" }}
+            activeStyle={{ fontWeight: "bold" }}
+          >
             <div>{item.name}</div>
           </NavLink>
         );
