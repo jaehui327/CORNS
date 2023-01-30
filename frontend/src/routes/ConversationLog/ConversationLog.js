ec8e0282ebf509f13f8033f6eeede17d6fc3f5d8
isks2 <isks2@naver.com>
2023-01-30 17:11:07 +0900

    🎨UI-publishing self_evaluation

--- frontend/src/routes/ConversationLog/ConversationLog.js
+++ frontend/src/routes/ConversationLog/ConversationLog.js
@@ -4,7 +4,7 @@ import Sidebar from "../../components/GlobalComponents/Sidebar";
 import Log from "./Log";
 import LogBookmark from "./LogBookmark";
 import LogDetail from "./LogDetail";
-import LogWord from './LogWord';
+import LogWord from "./LogWord";
 import Grid from "@mui/material/Grid";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
@@ -14,17 +14,17 @@ function ConversationLog({}) {
     { name: "쫑알로그", route: "/conversationLog/loglist" },
     { name: "즐겨찾기", route: "/conversationLog/bookmarks" },
     { name: "쫑알단어", route: "/conversationLog/words" },
-
   ];
 
   return (
     <div
-    css={css`
-      margin: 0 105px;
-    `}>
+      css={css`
+        margin: 0 105px;
+      `}
+    >
       <Navbar />
 
-      <Grid container spacing={1} sx={{margin: '124px 0 0 0'}}>
+      <Grid container spacing={1} sx={{ margin: "124px 0 0 0" }}>
         <Grid item xs={2}>
           <Sidebar Items={SidebarItems} />
         </Grid>
@@ -34,6 +34,7 @@ function ConversationLog({}) {
               box-sizing: border-box;
               border: 3px solid #111111;
               padding: 10px 32px 32px 32px;
+              position: relative;
             `}
           >
             <Switch>
@@ -48,11 +49,7 @@ function ConversationLog({}) {
                 path="/conversationLog/logdetail/:room_no"
                 component={LogDetail}
               />
-              <Route
-                exact
-                path="/conversationLog/words"
-                component={LogWord}
-              />
+              <Route exact path="/conversationLog/words" component={LogWord} />
             </Switch>
           </div>
         </Grid>
