21a6558b7a06e10743a6843eb340c9fc029dc5f3
isks2 <isks2@naver.com>
2023-01-31 15:42:21 +0900

    ✅add friends_carousel

--- frontend/src/components/Community/FriendList.js
+++ frontend/src/components/Community/FriendList.js
@@ -1,60 +1,64 @@
-import React from 'react';
-import SearchComp from '../GlobalComponents/SearchComp';
-import UserList from './UserList';
+import React from "react";
+import SearchComp from "../GlobalComponents/SearchComp";
+import UserList from "./UserList";
 
-import Box from "@mui/material/Box";
+import { Box } from "@mui/material";
 
-function FriendList({items}) {
+function FriendList({ items }) {
   const users = [
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
     {
-      img_url: "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
+      img_url:
+        "https://i.pinimg.com/564x/af/7b/de/af7bde50489a2cb932a98741b877704b.jpg",
       nickname: "isk2",
       user_id: 1000,
       level: 10,
     },
+  ];
 
-  ]
-  
   return (
     <>
       <h3>친구목록</h3>
       <SearchComp />
-      <Box  padding="48px 112px">
-        <UserList items = {users}/>
+      <Box padding="48px 112px">
+        <UserList items={users} />
       </Box>
-      
     </>
-  )
+  );
 }
 
 export default FriendList;
