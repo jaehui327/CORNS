e6105f16c5efbbe1b40dd3201930de23cbfb6f0c
dlgkfla213 <dlgkfla213@naver.com>
2023-01-31 23:50:02 +0900

    ⚡️️[BE] growth - ExpList 출력, Pageable 구현중

--- backend/src/main/java/com/w6w/corns/domain/explog/ExpLogRepository.java
+++ backend/src/main/java/com/w6w/corns/domain/explog/ExpLogRepository.java
@@ -1,8 +1,14 @@
 package com.w6w.corns.domain.explog;
 
+import org.hibernate.annotations.Where;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExpLogRepository extends JpaRepository<ExpLog, Integer> {
+
+//    Slice<ExpLog> findByUserIdAndRegTmLessThan(int userId, Pageable pageable, String baseTime);
+    Slice<ExpLog> findByUserId(int userId, Pageable pageable);
 }
--- backend/src/main/java/com/w6w/corns/service/growth/GrowthService.java
+++ backend/src/main/java/com/w6w/corns/service/growth/GrowthService.java
@@ -1,8 +1,16 @@
 package com.w6w.corns.service.growth;
 
+import com.w6w.corns.dto.explog.ExpLogResponseDto;
 import com.w6w.corns.dto.level.LevelDto;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface GrowthService {
+    //경험치 상위 백분위 계산
     int calExpPercentile(int userId) throws Exception;
+    //유저의 레벨 정보 반환
     LevelDto getUserLevel(int userId) throws Exception;
+    //유저의 경험치 목록 반환
+    List<ExpLogResponseDto> getExpLogList(int userId, Pageable pageable) throws Exception;
 }
