f7c40e10b52cbcc9383a335f87562f0a4cdd3b48
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-01-31 14:41:52 +0900

    ✅ Add user name tag component

---
+++ frontend/src/components/GlobalComponents/UserNameTag.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import UserProfileModal from "components/GlobalComponents/UserProfileModal";
+import { Box, Modal } from "@mui/material";
+
+function UserNameTag({ nickname, user_id }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Box onClick={handleOpen} sx={{ cursor: "pointer" }}>
+        {nickname}#{user_id}
+      </Box>
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box>
+          <UserProfileModal />
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+export default UserNameTag;
