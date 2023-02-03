0754c6ee5395efcd2de60af6b53052b45fb31486
dlgkfla213 <dlgkfla213@naver.com>
2023-02-03 14:19:02 +0900

    ⚡️[BE] jwt Handler 설정 완료 및 변수명 변경

--- backend/src/main/java/com/w6w/corns/config/WebConfiguration.java
+++ backend/src/main/java/com/w6w/corns/config/WebConfiguration.java
@@ -16,6 +16,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .maxAge(3000);
     }
