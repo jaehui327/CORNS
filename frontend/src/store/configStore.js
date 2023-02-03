0ca2f7f6c71726166fbb7e116ecbae5483bc1b4d
isks2 <isks2@naver.com>
2023-02-03 17:51:07 +0900

    ⚡️[FE]subjectBtn redux action 연결중

--- frontend/src/store/configStore.js
+++ frontend/src/store/configStore.js
@@ -7,10 +7,12 @@ import { logger } from "redux-logger";
 import thunk from "redux-thunk";
 import { roomListReducer } from "store/reducers/roomListReducer";
 import { subjectsReducer } from "store/reducers/subjectsReducer";
+import { roomFilterReducer } from "store/reducers/roomFilterReducer";
 
 const rootReducer = combineReducers({
   roomListReducer: roomListReducer.reducer,
   subjectsReducer: subjectsReducer.reducer,
+  roomFilterReducer: roomFilterReducer.reducer,
 });
 
 export const store = configureStore(
