07a2b49936fc3b2f2ba99d30b0834e074cf0a3f2
isks2 <isks2@naver.com>
2023-02-06 08:58:47 +0900

    ⚡️[FE]roomListReducer add, & remove 수정

--- frontend/src/store/reducers/roomListReducer.js
+++ frontend/src/store/reducers/roomListReducer.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const getRoomList = createAsyncThunk("GET_ROOM_LIST", async () => {
-  const response = await axios.get(`${process.env.REACT_APP_HOST}/room`);
+  const response = await axios.get(
+    `${process.env.REACT_APP_HOST}/room?page=1&size=18&basetime=2022-0205 00:00:00&subject=1%2%3%4%5&minTime=5&maxTime=30&showAvail=true`
+  );
   return response.data.rooms;
 });
 
