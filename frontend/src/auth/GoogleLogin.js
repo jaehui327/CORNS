d0c10cb01aad2672e2e0d13160ddbd5b3f7339ce
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-02-06 10:32:47 +0900

    ⚡ [FE] GOOGLE LOGIN error

--- frontend/src/auth/GoogleLogin.js
+++ frontend/src/auth/GoogleLogin.js
@@ -6,7 +6,18 @@ const GoogleLogin = async (code) => {
     const response = await axios.get(
       `${process.env.REACT_APP_HOST}/user/auth/google/callback?code=${code}`
     );
-    console.log(response);
+    if (response.status === 200) {
+      // 로그인 성공
+      sessionStorage.setItem("accessToken", response.data.accessToken);
+      sessionStorage.setItem("refreshToken", response.data.refreshToken);
+      sessionStorage.setItem("userId", response.data.loginUser.userId);
+      sessionStorage.setItem("imgUrl", response.data.loginUser.imgUrl);
+      sessionStorage.setItem("email", response.data.loginUser.email);
+      sessionStorage.setItem("nickname", response.data.loginUser.nickname);
+
+      // Home 페이지로 이동
+      window.location.href = '/'
+    }
   } catch (e) {
     console.log(e);
   }
