8799c976c5893be27fe5719a7943516134b23235
dlgkfla213 <dlgkfla213@naver.com>
2023-02-07 17:58:39 +0900

    ⚡️[BE] 파일 경로 수정중

--- backend/src/main/java/com/w6w/corns/controller/FileController.java
+++ backend/src/main/java/com/w6w/corns/controller/FileController.java
@@ -1,7 +1,6 @@
 package com.w6w.corns.controller;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,29 +8,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/test")
 @ApiOperation("file")
 public class FileController {
 
-    @Value("{upload.path.upload-im}")
-    private String uploadPath;
-
     @PostMapping
-    public ResponseEntity sendFile(int userId, @RequestPart MultipartFile file) throws IOException {
+    public ResponseEntity sendFile(int userId, @RequestParam MultipartFile file) throws IOException {
 
         String originalFileName = file.getOriginalFilename();
 
         String saveUrl = "/home/ubuntu/uploads/"+userId+"/"+originalFileName;
         System.out.println("saveUrl = " + saveUrl);
 
-        File dest = new File(saveUrl);
+        String path = System.getProperty("user.dir")+File.separator+"/home/ubuntu/uploads/";
+        System.out.println("path = " + path);
 
-//        File dest = new File("/home/ubuntu/uploads/"+userId+"/"+originalFileName);
+        File dest = new File(path);
+        
+        if(!dest.exists()){
+            if(dest.mkdir()) System.out.println("폴더 생성 성공");
+            else System.out.println("실패");
+        }else System.out.println("이미존재");
+ 
+    
+    //        File dest = new File("/home/ubuntu/uploads/"+userId+"/"+originalFileName);
 
         file.transferTo(dest);
 
