8cd77e6c25c8095bda3ac991994be5f0ee0a4d65
dlgkfla213 <dlgkfla213@naver.com>
2023-02-07 13:44:37 +0900

    ⚡️[BE] img 업로드 설정중

--- backend/src/main/java/com/w6w/corns/config/WebConfiguration.java
+++ backend/src/main/java/com/w6w/corns/config/WebConfiguration.java
@@ -2,13 +2,17 @@ package com.w6w.corns.config;
 
 import com.w6w.corns.jwt.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -16,6 +20,19 @@ import java.util.Map;
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final JwtInterceptor jwtInterceptor;
+    @Value("${custom.path.upload-img}")
+    private String uploadImagesPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("file:///" + uploadImagesPath + "/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
