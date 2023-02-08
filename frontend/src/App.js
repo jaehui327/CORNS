bbe1aa90d21c4ab67f9aa514eb9ef9a9feecc158
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-02-08 11:04:55 +0900

    ✅ [FE] add AuthRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React, { Route, Switch, Redirect } from "react-router-dom";
-import Home from "./routes/Home";
-import Conversation from "./routes/Conversation";
-import ConversationLog from "./routes/ConversationLog/ConversationLog";
-import Community from "./routes/Community/Community";
-import GrowthRecord from "./routes/GrowthRecord/GrowthRecord";
-import Login from "./routes/LogIn";
-import Signin from "./routes/SignIn";
-import Mypage from "./routes/MyPage/MyPage";
-import Room from "./routes/Room/Room";
-import NotFound from "./routes/NotFound";
+import AuthRoute from "routes/AuthRoute";
+
+import Home from "routes/Home";
+import Conversation from "routes/Conversation";
+import ConversationLog from "routes/ConversationLog/ConversationLog";
+import Community from "routes/Community/Community";
+import GrowthRecord from "routes/GrowthRecord/GrowthRecord";
+import Login from "routes/LogIn";
+import Signin from "routes/SignIn";
+import Mypage from "routes/MyPage/MyPage";
+import Room from "routes/Room/Room";
+import NotFound from "routes/NotFound";
 import TopBtn from "components/GlobalComponents/TopBtn";
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 
 function App() {
   return (
@@ -19,13 +19,13 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/conversation" component={Conversation} />
-        <Route path="/conversationLog" component={ConversationLog} />
-        <Route path="/community" component={Community} />
-        <Route path="/growthRecord" component={GrowthRecord} />
+        <AuthRoute path="/conversationLog" component={ConversationLog} />
+        <AuthRoute path="/community" component={Community} />
+        <AuthRoute path="/growthRecord" component={GrowthRecord} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signin" component={Signin} />
-        <Route path="/mypage" component={Mypage} />
-        <Route path="/room" component={Room} />
+        <AuthRoute path="/mypage" component={Mypage} />
+        <AuthRoute path="/room" component={Room} />
         <Route exact path="/NotFound" component={NotFound} />
         <Redirect to="/NotFound" />
       </Switch>
