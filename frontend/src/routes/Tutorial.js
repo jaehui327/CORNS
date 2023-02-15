021c2d54c6194a530ad7e3d81ccc86064439b9bc
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-02-15 20:52:48 +0900

    ✅[FE] Tutorial

--- frontend/src/routes/Tutorial.js
+++ frontend/src/routes/Tutorial.js
@@ -1,6 +1,14 @@
 import Navbar from "components/GlobalComponents/Navbar";
 import React from "react";
 import Footer from "components/GlobalComponents/Footer";
+
+import tutorial1 from "assets/tutorial1.png";
+import tutorial2 from "assets/tutorial2.png";
+import tutorial3 from "assets/tutorial3.png";
+import tutorial4 from "assets/tutorial4.png";
+
+import { Box } from "@mui/material";
+
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
@@ -16,13 +24,49 @@ function Tutorial() {
         `}
       >
         <Navbar />
-        <div
-          css={css`
-            margin: 124px 0 0 0;
-          `}
+        <Box
+          sx={{
+            marginTop: "0px",
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "0",
+            textAlign: "center",
+          }}
         >
-          <h1>Tutorial</h1>
-        </div>
+          <img
+            src={tutorial1}
+            alt="tutorial1"
+            css={css`
+              width: 80%;
+            `}
+          />
+          <img
+            src={tutorial2}
+            alt="tutorial2"
+            css={css`
+              width: 80%;
+            `}
+          />
+
+          <img
+            src={tutorial3}
+            alt="tutorial3"
+            css={css`
+              width: 80%;
+            `}
+          />
+
+          <img
+            src={tutorial4}
+            alt="tutorial4"
+            css={css`
+              width: 80%;
+            `}
+          />
+        </Box>
       </div>
       <Footer />
     </div>
